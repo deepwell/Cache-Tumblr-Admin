0d073869623796e8cf1dd976d783a5bb4fea9f9d
Mark Deepwell <mmmark@gmail.com>
2011-11-17 16:00:01 -0800

    Check for no files

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,7 +1,11 @@
 <h2>Cached files</h2>
 
-<ul>
-<?php foreach ($files as $file): ?>
-  <li><a href="/uploads/<?php echo $file ?>"><?php echo $file ?></a></li>
-<?php endforeach ?>
-</ul>
+<?php if (count($files) <= 0): ?>
+  <p>No files cached yet.</p>
+<?php else: ?>
+  <ul>
+  <?php foreach ($files as $file): ?>
+    <li><a href="/uploads/<?php echo $file ?>"><?php echo $file ?></a></li>
+  <?php endforeach ?>
+  </ul>
+<?php endif ?>
